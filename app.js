74a61762f4bf6ee64966e7622c73a819cbd08e70
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 13:10:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
 const db = require('./db');
 
 const authController = require('./authController');
