d1970f274c4e96f97db7ce441a9d4a38d959d37d
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 13:28:28 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,4 +4,12 @@ const sequelize = new Sequelize(process.env.DB_CONNECT, {
   dialect: 'mysql'
 });
 
+sequelize.authenticate()
+.then(() => {
+  console.log('Connected to the database');
+})
+.catch((err) => {
+  console.log('Unable to connect to the database:', err);
+});
+
 module.exports = sequelize;
