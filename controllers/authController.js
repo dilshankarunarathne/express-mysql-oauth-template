f49b9c776d64686632122ff16142b278c5e22f46
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-17 13:11:25 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
 const User = require('./User');
 const multer = require('multer');
 const upload = multer();
